9ce61a5a42d5c654be1fb0089bb6653bfd88c434
SAYAN MONDAL <smondal254842@gmail.com>
2023-09-26 19:37:05 +0530

    first commit

---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+    return (
+        <>
+    <nav>
+        <div className='logo'>
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li herf="#">Menu</li>
+          <li herf="#">Location</li>
+          <li herf="#">About</li>
+          <li herf="#">Contact</li>
+        </ul>
+        <button>Login</button>
+    </nav> 
+      </>
+    )
+}
+
+export default Navigation;
